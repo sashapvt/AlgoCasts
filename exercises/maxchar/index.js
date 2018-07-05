b798d1b6c9dec8525617c03defd8f3f81780d060
Sasha Shvorob <sashashvorob@gmail.com>
2018-07-05 13:35:23 +0300

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+console.log(maxChar("apple 1231111"));
+
+function maxChar(str) {
+    let map = {};
+    let maxVal = 0;
+    let maxChar = '';
+    for (let c of str) {
+        map[c] = map[c] ? map[c] + 1 : 1;
+    }
+    for (let m in map) {
+        if (map[m] > maxVal) {
+            maxVal = map[m];
+            maxChar = m;
+        }
+    }
+    return maxChar;
+}
 
 module.exports = maxChar;
