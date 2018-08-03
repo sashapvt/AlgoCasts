45abedbf8028b0e253955ec639322ad72df98466
Sasha Shvorob <sashashvorob@gmail.com>
2018-08-03 18:32:01 +0300

    anagrams with char maps

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -11,11 +11,34 @@
 console.log(anagrams('RAIL! SAFETY!', 'fairy tales'));
 
 function anagrams(stringA, stringB) {
-    const stringA2 = stringA.replace(/[^\w]/g, "").toUpperCase().split('').sort().join('');
-    const stringB2 = stringB.replace(/[^\w]/g, "").toUpperCase().split('').sort().join('');
-    return stringA2 == stringB2;
-    console.log(stringA2);
-    console.log(stringB2);
+    const mapA = buildCharMap(stringA);
+    const mapB = buildCharMap(stringB);
+
+    if(Object.keys(mapA).length !== Object.keys(mapB).length) return false;
+
+    for (let c in mapA) {
+        if (mapA[c] !== mapB[c]) return false;
+    }
+
+    return true;
+}
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for(let c of str.replace(/[^\w]/g, "").toLowerCase()) {
+        charMap[c] = charMap[c] + 1 || 1;
+    }
+
+    return charMap;
 }
 
 module.exports = anagrams;
+
+// function anagrams(stringA, stringB) {
+//     const stringA2 = stringA.replace(/[^\w]/g, "").toUpperCase().split('').sort().join('');
+//     const stringB2 = stringB.replace(/[^\w]/g, "").toUpperCase().split('').sort().join('');
+//     return stringA2 == stringB2;
+//     console.log(stringA2);
+//     console.log(stringB2);
+// }
