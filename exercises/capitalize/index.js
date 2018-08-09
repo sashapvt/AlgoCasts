9fc4b8bce233025ae47a475ed83661388c485c6f
Sasha Shvorob <sashashvorob@gmail.com>
2018-08-09 09:56:00 +0300

    capitalize my solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,15 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+console.log(capitalize('a short sentence'));
+
+function capitalize(str) {
+    const words = str.split(' ');
+    for(let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+
+    return words.join(' ');
+}
 
 module.exports = capitalize;
