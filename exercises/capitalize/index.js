a22152fe68ff7f31fd1b5472f8ef9bc17f124444
sashapvt <sashapvt@gmail.com>
2018-08-18 12:06:46 +0300

    capitalize2

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -10,12 +10,21 @@
 console.log(capitalize('a short sentence'));
 
 function capitalize(str) {
-    const words = str.split(' ');
-    for(let i = 0; i < words.length; i++) {
-        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    const words = [];
+    for(let word of str.split(' ')) {
+        words.push(word[0].toUpperCase() + word.slice(1));
     }
 
     return words.join(' ');
 }
 
 module.exports = capitalize;
+
+// function capitalize(str) {
+//     const words = str.split(' ');
+//     for(let i = 0; i < words.length; i++) {
+//         words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+//     }
+
+//     return words.join(' ');
+// }
