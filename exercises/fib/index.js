bf260f634423d1cb2872bbb00151e4e53fe8865d
Sasha Shvorob <sashashvorob@gmail.com>
2018-11-14 09:40:53 +0200

    cached fib

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -8,13 +8,37 @@
 // Example:
 //   fib(4) === 3
 
-console.log(fib(1));
+function memoize(fn) {
+    const cache = {};
+    return function(...args) {
+      if (cache[args]) {
+        return cache[args];
+      }
+  
+      const result = fn.apply(this, args);
+      cache[args] = result;
+  
+      return result;
+    };
+  }
+  
+  function slowFib(n) {
+    if (n < 2) {
+      return n;
+    }
+  
+    return fib(n - 1) + fib(n - 2);
+  }
+  
+  const fib = memoize(slowFib);
+  
+  
 
-function fib(n) {
-    if (n < 2) return n;
+// function fib(n) {
+//     if (n < 2) return n;
 
-    return fib(n-1) + fib(n-2);
-}
+//     return fib(n-1) + fib(n-2);
+// }
 
 // function fib(n) {
 //     if (n == 0) return 0;
