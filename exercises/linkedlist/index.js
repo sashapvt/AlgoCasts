bd35d320a06be14f022a7d6259c9f6b6e622b99a
sashapvt <sashapvt@gmail.com>
2018-12-01 18:17:35 +0200

    linkedlist work in progress

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,74 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
 
-class LinkedList {}
+class Node {
+    constructor (data, next = null) {
+        this.data = data;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+
+    insertFirst(data) {
+        this.head = new Node(data, this.head);
+    }
+
+    size() {
+        let s = 0
+        let n = this.head;
+        while(n) {
+            s++;
+            n = n.next;
+        }
+        return s;
+    }
+
+    getFirst() {
+        return this.head;
+    }
+
+    getLast() {
+        let n = this.head;
+        while(n && n.next) {
+            n = n.next;
+        }
+        return n;
+    }
+
+    clear() {
+        this.head = null;
+    }
+
+    removeFirst() {
+        if (this.head) {
+            this.head = this.head.next;
+        }
+    }
+
+    removeLast() {
+        if (!this.head) {
+            return;
+        }
+        if(!this.head.next) {
+            this.head = null;
+            return;
+        }
+        let n = this.head;
+        while(n.next && n.next.next) {
+            n = n.next;
+        }
+        n.next = null;
+    }
+
+    insertLast(data) {
+        if (!this.head) this.insertFirst(data);
+        this.getLast().next = new Node(data);
+    }
+}
 
 module.exports = { Node, LinkedList };
