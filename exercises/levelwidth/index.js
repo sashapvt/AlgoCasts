479a4c596ea988cc9fcee5d071557087f8387bfe
sashapvt <sashapvt@gmail.com>
2019-01-08 15:13:54 +0200

    levelWidth

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,34 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+const Node = require('./node');
+
+function levelWidth(root) {
+    let res = [];
+    let arr = [root, 's'];
+    let sum = 0;
+    while (arr.length) {
+        let node = arr.shift();
+        if (node === 's') {
+            res.push(sum);
+            sum = 0;
+            if (arr.length != 0) arr.push('s');
+        }
+        else {
+            sum++;
+            if (node.children) arr.push(...node.children);
+        }
+    }
+
+    return res;
+}
+
+const root = new Node(0);
+root.add(1);
+root.add(2);
+root.add(3);
+root.children[0].add(4);
+root.children[2].add(5);
+console.log(levelWidth(root));
 
 module.exports = levelWidth;
