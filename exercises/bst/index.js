2dc10756d81ed6b317776e8a60be345fd14176aa
sashapvt <sashapvt@gmail.com>
2019-01-24 18:30:49 +0200

    bst contains

--- exercises/bst/index.js
+++ exercises/bst/index.js
@@ -36,6 +36,27 @@ class Node {
             }
         }
     }
+
+    contains(data) {
+        if (data == this.data) {
+            return this;
+        }
+        else if (data < this.data) {
+            if (this.left) {
+                return (this.left.contains(data));
+            }
+            else {
+                return null;
+            }
+        } else {
+            if (this.right) {
+                return (this.right.contains(data));
+            }
+            else {
+                return null;
+            }
+        }
+    }
 }
 
 module.exports = Node;
