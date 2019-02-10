86532906fee5e4a99ee09b4e4b80cbaef8faf463
sashapvt <sashapvt@gmail.com>
2019-02-10 12:48:14 +0200

    validate

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -5,6 +5,20 @@
 // every node's right hand child is greater than
 // the parent
 
-function validate(node, min = null, max = null) {}
+function validate(node, min = null, max = null) {
+    if ((max && node.data > max) || (min && node.data < min)) return false;
+    if (node.left && !validate(node.left, min, node.data)) return false;
+    if (node.right && !validate(node.right, node.data, max)) return false;
+    return true;
+}
+
+const Node = require('./node');
+const n = new Node(10);
+n.insert(5);
+n.insert(15);
+n.insert(0);
+n.insert(20);
+
+console.log(validate(n));
 
 module.exports = validate;
